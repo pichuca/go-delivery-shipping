00cf63dc3d65e971cda704178d30a23db861ee94
Salvador Roibon <salvador.sroibon@mercadolibre.com>
2018-07-02 12:21:49 -0300

    Point to page public files.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
+var serveStatic = require('serve-static');
 var port = process.env.PORT || 3600;
 
+app.use(serveStatic('public', {'index': ['index.html', 'index.htm']}));
 app.listen(port);
 
-console.log('App server running on: ' + port); 
+console.log('App server running on: ' + port);
