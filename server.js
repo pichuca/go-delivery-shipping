b3c281f259ddb75f2c4358b48f9fce897b2fe312
Salvador Roibon <salvador.sroibon@mercadolibre.com>
2018-07-02 12:37:15 -0300

    Added custom css file. Changed serving static files strategy.

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var app = express();
-var serveStatic = require('serve-static');
 var port = process.env.PORT || 3600;
 
-app.use(serveStatic('public', {'index': ['index.html', 'index.htm']}));
+app.use(express.static(__dirname + '/public'));
 app.listen(port);
 
 console.log('App server running on: ' + port);
