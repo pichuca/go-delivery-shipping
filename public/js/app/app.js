7ac428bea3c7c2d3697d2e4fc5a31a1b419559fb
Salvador Roibon <salvador.sroibon@mercadolibre.com>
2018-07-06 01:30:07 -0300

    Removed sections. Added script for future redirect to tracker page.

---
+++ public/js/app/app.js
@@ -0,0 +1,9 @@
+console.log('Works!');
+
+(function(win, doc) {
+  var trackingButton = doc.querySelector('.tracking');
+  function onTrackingClick() {
+    alert('Redirect to tracker page. WIP (Work In Progress.)');
+  }
+  trackingButton.addEventListener('click', onTrackingClick, false);
+})(window, document);
